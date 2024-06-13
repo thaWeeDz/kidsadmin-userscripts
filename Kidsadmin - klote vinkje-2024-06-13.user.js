949d533e20bab98dd71d199c4b619e1f9e58799c
Wytze Voerman <wytze@relaxtension.nl>
2024-06-13 15:43:03 +0200

    Create Kidsadmin - klote vinkje-2024-06-13.user.js
    Initial commit op checkbox script

---
+++ Kidsadmin - klote vinkje-2024-06-13.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Kidsadmin - klote vinkje
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-13
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.kidsadminlogin.nl/invoer/childfamily/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kidsadminlogin.nl
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Hello you!");
+
+    // Zoek de button met de class 'schema-button'
+    const checkBtnLoaded = () => {
+        const icon = document.querySelector('.btn i.fa-cog')
+        if (icon) {
+            const btn = icon.closest('.btn');
+            btn.addEventListener('click', () => {
+                console.log("click works");
+                // Functie om te controleren of het element met class 'overzicht' aanwezig is
+                const checkRoosterLoaded = () => {
+                    const roosterElement = document.querySelector('.schedule-row.edit');
+                    if (roosterElement) {
+                        // Voer hier je JS bewerking uit
+                        console.log('Overzicht element is geladen:', roosterElement);
+
+                        // Voorbeeldbewerking: verander de achtergrondkleur
+                        roosterElement.querySelector(".check-box").checked = false;
+                    } else {
+                        // Probeer het opnieuw na een korte tijd
+                        setTimeout(checkRoosterLoaded, 100);
+                    }
+                };
+                // Start met controleren of het 'overzicht' element is geladen
+                checkRoosterLoaded();
+            });
+        }else{
+            // Probeer het opnieuw na een korte tijd
+            setTimeout(checkBtnLoaded, 100);
+        }
+    };
+    checkBtnLoaded();
+})();
